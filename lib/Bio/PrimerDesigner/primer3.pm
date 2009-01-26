9253f6140039bc7f541f4a7eeb39b93dfa67c0f0
Sheldon McKay <mckays@cshl.edu>
2009-01-26 14:25:56 +0000

    Import of SMCKAY/Bio-PrimerDesigner-0.05 from CPAN.
    gitpan-cpan-distribution: Bio-PrimerDesigner
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         SMCKAY/Bio-PrimerDesigner-0.05.tar.gz
    gitpan-cpan-author:       SMCKAY
    gitpan-cpan-maturity:     released

--- lib/Bio/PrimerDesigner/primer3.pm
+++ lib/Bio/PrimerDesigner/primer3.pm
@@ -540,7 +540,7 @@ Ken Youens-Clark E<lt>kclark@cpan.orgE<gt>.
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation; version 2.
+the Free Software Foundation; version 3 or any later version.
 
 This program is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -549,7 +549,7 @@ General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
 USA.
 
 =head1 SEE ALSO
